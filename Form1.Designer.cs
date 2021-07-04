bce1f81a0a8eb58ca73d435dc18349bb61c734e3
Zielony20 <piotrsikora98@gmail.com>
2021-07-04 20:39:27 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.crypt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.deCode = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // crypt
+            // 
+            this.crypt.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("crypt.BackgroundImage")));
+            this.crypt.Location = new System.Drawing.Point(274, 244);
+            this.crypt.Name = "crypt";
+            this.crypt.Size = new System.Drawing.Size(136, 65);
+            this.crypt.TabIndex = 0;
+            this.crypt.Text = "Szyfruj";
+            this.crypt.UseVisualStyleBackColor = true;
+            this.crypt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(40, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(845, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0001001100110100010101110111100110011011101111001101111111110001";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Location = new System.Drawing.Point(40, 27);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(845, 137);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.deCode);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.crypt);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(29, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(925, 326);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(40, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wiadomość";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(40, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Klucz";
+            // 
+            // deCode
+            // 
+            this.deCode.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.deCode.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("deCode.BackgroundImage")));
+            this.deCode.Location = new System.Drawing.Point(503, 244);
+            this.deCode.Name = "deCode";
+            this.deCode.Size = new System.Drawing.Size(136, 65);
+            this.deCode.TabIndex = 3;
+            this.deCode.Text = "Rozszyfruj";
+            this.deCode.UseVisualStyleBackColor = true;
+            this.deCode.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox4.Location = new System.Drawing.Point(40, 90);
+            this.textBox4.MaxLength = 64;
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(845, 148);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "z[:ÞHÑ<ã¨×O\"H$Àñpmq<PÉêPb*+¥¨u²Òÿ";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_2);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel3.Controls.Add(this.textBox3);
+            this.panel3.Location = new System.Drawing.Point(29, 400);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(925, 186);
+            this.panel3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(984, 607);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "DES";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button crypt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button deCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using WinFormsApp1.Properties;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+      
+        }
+
+        DataEncryptionStandard p = new DataEncryptionStandard();
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < textBox1.Text.Length; i++)
+                if (textBox1.Text[i] != '1' && textBox1.Text[i] != '0') return;
+            if (textBox1.Text.Length == 64)
+            {
+                textBox3.Text = p.Des(Convert.ToString(textBox4.Text), Convert.ToUInt64(textBox1.Text, 2), true);
+                
+            }
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for(int i=0; i< textBox1.Text.Length; i++)
+            if (textBox1.Text[i] != '1' && textBox1.Text[i] != '0') return;
+            if (textBox1.Text.Length == 64)
+            {
+                textBox3.Text = p.Des(textBox4.Text, Convert.ToUInt64(textBox1.Text, 2), false);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+
+}
+
+
